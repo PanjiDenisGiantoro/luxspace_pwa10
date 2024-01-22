c428ed055357b6fe6237a9f1ab72a8b076ff54f0
PanjiDenisGiantoro <panjidenisgiantoroo@gmail.com>
2024-01-22 20:51:17 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -8,52 +8,61 @@ import AsideMenu from "./components/AsideMenu";
 import Footer from "./components/Footer";
 import Clients from "./components/Clients";
 import Offline from "./components/Offline";
+import Splash from "./pages/Splash";
 
 
 function App() {
     const [items, setItems] = React.useState([]);
     const [offlineStatus, setOfflineStatus] = React.useState(!navigator.onLine);
 
+    const [isLoading, setIsLoading] = React.useState(true);
+
     function handleOfflineStatus() {
         setOfflineStatus(!navigator.onLine);
     }
-    React.useEffect(function (){
-        (async function(){
-            const response = await fetch('https://bwacharity.fly.dev/items',{
+
+    React.useEffect(function () {
+        (async function () {
+            const response = await fetch('https://bwacharity.fly.dev/items', {
                 headers: {
                     "Content-Type": "application/json",
                     "Accept": "application/json"
                 }
             });
-            const { nodes } = await response.json();
+            const {nodes} = await response.json();
             setItems(nodes);
-            const script = document.createElement("script");
-            script.src = "/carousel.js";
-            script.async = false;
-            document.body.appendChild(script);
+
 
             handleOfflineStatus();
             window.addEventListener("online", handleOfflineStatus);
             window.addEventListener("offline", handleOfflineStatus);
 
-            return function() {
+            setTimeout(function () {
+                setIsLoading(false);
+            }, 1500)
+            return function () {
                 window.removeEventListener("online", handleOfflineStatus);
                 window.removeEventListener("offline", handleOfflineStatus);
             }
         })();
-    },[offlineStatus])
-  return (
-      <>
-          {offlineStatus && <Offline/>}
-        <Header/>
-        <Hero/>
-        <Browser/>
-        <Arrived items={items}/>
-        <Clients />
-        <AsideMenu />
-        <Footer />
-      </>
-  );
+    }, [offlineStatus])
+    return (
+        <>
+            {isLoading === true ? <Splash/> :
+                (
+                    <>
+                        {offlineStatus && <Offline/>}
+                        <Header/>
+                        <Hero/>
+                        <Browser/>
+                        <Arrived items={items}/>
+                        <Clients/>
+                        <AsideMenu/>
+                        <Footer/>
+
+                    </>)}
+        </>
+    );
 }
 
 export default App;
---
+++ src/pages/Splash.js
@@ -0,0 +1,17 @@
+function Splash()
+{
+    return (
+        <section>
+            <div className="container mx-auto min-h-screen">
+                <div className="flex flex-col items-center justify-center h-screen">
+                    <div className="w-full md:w-4/12 text-center">
+                        <p className="mb-16 px-4">
+                            Kami menyediakan furniture berkelas yang membuat ruangan terasa homey
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default Splash;
